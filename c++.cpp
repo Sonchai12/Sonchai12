72eef201dac9bc33cbe71627979a6d787d769317
Sonchai12 <139834072+Sonchai12@users.noreply.github.com>
2023-07-18 12:18:56 +0700

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+    float x=0,a=10,b7,c3,d6,e1,f2;
+    //x = a/f-c*b+e;
+    //x =e*b*d-f%a;
+    //x =a-b*e/b%d;
+    //x =
+    cout << x;
+    return(0);
+
+}
